6380fc9b3669a92fe6dbf195f3c01e9571093557
Jaisurya <262jaisuryas@gmail.com>
2025-12-14 14:27:59 +0530

    Added the assests and Navigation bar for the web

--- Vite-Frontend/src/App.jsx
+++ Vite-Frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Routes } from 'react-router'
+import { Routes , Route } from 'react-router'
 import Home from './pages/Home'
 import Collection from './pages/Collection'
 import About from './pages/About'
